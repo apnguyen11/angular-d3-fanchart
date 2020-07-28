69a5b4fda38ad2e7432e4dff518977df39137617
Andy Nguyen <andy.nguyen@aquila-system.com>
2020-07-28 13:39:38 -0500

    animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import * as d3 from 'd3';
 import * as data1 from './NLI_Dec_12_Pontus_HP.json';
 import * as data2 from './NLI_Prediction_Dec_12_Pontus.json';
 import * as dataLP from './NLI_Dec_12_Pontus_LP.json';
+import { Observable } from 'rxjs';
 
 interface dataConfig {
   date: Date;
@@ -40,45 +41,53 @@ export class AppComponent {
   data = this.dataJSON;
   dataPrediction = this.dataJSON2;
   LPData = this.dataLPJSON;
-  public isLP: boolean = false;
-  public isHP: boolean = true;
+  data2 = []
+  dataPrediction2 = []
+  sub:any
+  index = 1
+  index2 = 1
 
   constructor() {
-    console.log(this.data)
+
   }
 
   ngOnInit() {
-    console.log(new Date(this.data[0].NLI_time));
-    console.log(this.dataPrediction, '***');
-    const containerDiv = this.container.nativeElement;
-    if (containerDiv.childNodes[0]) {
+    console.log( this.dataPrediction)
+    this.sub = Observable.interval(1000)
+    .subscribe((val) => {
+   
+        this.data2.push(this.data[this.index])
+        if(new Date(this.data[this.index].NLI_time) >= new Date(this.dataPrediction[this.index2].TimeStamp)){
+          this.dataPrediction2.push(this.dataPrediction[this.index2])
+          this.index2 ++
+        }
+        this.index += 1
+        console.log(this.index)
+        // this.dataPrediction2.push(this.dataPrediction[i])
+      
+    
+      // console.log(this.XLPData)
+      const containerDiv = this.container.nativeElement;
+      if (containerDiv.childNodes[0]) {
+     
       containerDiv.removeChild(containerDiv.childNodes[0]);
-    }
-    containerDiv.appendChild(this.createChart());
-  }
+      }
+      if (containerDiv.childNodes[1]) {
+        containerDiv.removeChild(containerDiv.childNodes[1]);
+        }
+ 
+      containerDiv.appendChild(this.createChart());
+      
+      });
 
-  createHP() {
-    this.isHP = true;
-    this.isLP = false;
-
-    const containerDiv = this.container.nativeElement;
-    if (containerDiv.childNodes[0]) {
-      containerDiv.removeChild(containerDiv.childNodes[0]);
-    }
-    containerDiv.appendChild(this.createChart());
-    console.log('clicked');
-  }
-
-  createLP() {
-    this.isHP = false;
-    this.isLP = true;
-    const containerDiv = this.container.nativeElement;
-    if (containerDiv.childNodes[0]) {
-      containerDiv.removeChild(containerDiv.childNodes[0]);
-    }
-    console.log('clicked');
-    containerDiv.appendChild(this.createChart2());
-    console.log(containerDiv);
+    
+  //   console.log(new Date(this.data[0].NLI_time));
+  //   console.log(this.dataPrediction, '***');
+  //   const containerDiv = this.container.nativeElement;
+  //   if (containerDiv.childNodes[0]) {
+  //     containerDiv.removeChild(containerDiv.childNodes[0]);
+  //   }
+  //   containerDiv.appendChild(this.createChart());
   }
 
   createChart() {
@@ -114,11 +123,12 @@ export class AppComponent {
           g
             .append('text')
             .text('Time')
+            .attr('font-size', 35)
             .attr('text-anchor', 'end')
-            .attr('x', 390)
+            .attr('x', 455)
             .attr('fill', 'white')
             .attr('stroke', 'white')
-            .attr('y', 35),
+            .attr('y', 52),
         );
 
     let yAxis = g =>
@@ -139,6 +149,7 @@ export class AppComponent {
             .attr('fill', 'white')
             .attr('color', 'white')
             .attr('stroke', 'white')
+            .attr('font-size', 35)
             .attr('text-anchor', 'start')
             .text('NLI'),
         );
@@ -156,7 +167,10 @@ export class AppComponent {
             .attr('x1', d => 0.5 + x(d))
             .attr('x2', d => 0.5 + x(d))
             .attr('y1', margin.top)
-            .attr('y2', height - margin.bottom),
+            .attr('y2', height - margin.bottom)
+            .selectAll('text')
+            .attr('font-size', 35)
+            ,
         )
         .call(g =>
           g
@@ -206,13 +220,13 @@ export class AppComponent {
       .append('svg')
       .attr('viewBox', '-90, -70, 1000, 700')
       .attr('font-family', 'sans-serif')
-      .attr('font-size', 28)
+      .attr('font-size', 50)
       .attr('stroke', 'grey')
       .attr('stroke-miterlimit', 1);
 
-    svg.append('g').call(xAxis);
+    svg.append('g').call(xAxis).style('font', '15px helvetica');;
 
-    svg.append('g').call(yAxis);
+    svg.append('g').call(yAxis).style('font', '15px helvetica');;
 
     svg.append('g').call(grid);
 
@@ -220,20 +234,20 @@ export class AppComponent {
       .append('path')
       .attr('fill', '#5CD149')
       .attr('fill-opacity', 0.2)
-      .attr('d', area1(this.dataPrediction.slice(0, this.dataPrediction.length)));
+      .attr('d', area1(this.dataPrediction.slice(0, this.dataPrediction2.length)));
 
     svg
       .append('path')
       .attr('fill', '#216716')
       .attr('fill-opacity', 0.2)
-      .attr('d', area2(this.dataPrediction.slice(0, this.dataPrediction.length)));
+      .attr('d', area2(this.dataPrediction.slice(0, this.dataPrediction2.length)));
 
     svg
       .append('path')
       .attr('fill', 'none')
       .attr('stroke', 'steelblue')
       .attr('stroke-width', 4)
-      .attr('d', line(this.data.slice(0, this.data.length)));
+      .attr('d', line(this.data.slice(0, this.data2.length)));
 
     svg
       .append('path')
@@ -255,140 +269,8 @@ export class AppComponent {
       .attr('stroke', 'white')
       .attr('stroke-width', 1.5)
       .attr('stroke-dasharray', '3,3')
-      .attr('d', predictionLine(this.dataPrediction));
+      .attr('d', predictionLine(this.dataPrediction.slice(0, this.dataPrediction2.length)));
 
     return nliDiv;
+        } 
   }
-
-  createChart2() {
-    const nliDiv = document.createElement('div');
-    const nliGraph = d3.select(nliDiv).classed('nli-chart-wrapper', true);
-    let margin = { top: 20, right: 30, bottom: 30, left: 40 };
-    let height = 600;
-    let width = 800;
-
-    let x = d3
-      .scaleUtc()
-      .domain(d3.extent(this.LPData, (d: any) => new Date(d.NLI_time)))
-      .rangeRound([margin.left, width - margin.right]);
-
-    let y = d3
-      .scaleLinear()
-      .domain([0.1, d3.max(this.data, (d: any) => parseFloat(d.NLI))])
-      .rangeRound([height - margin.bottom, margin.top])
-      .clamp(true);
-
-    // d3
-    //   .scaleLog()
-    //   .domain([0.1, d3.max(this.data, (d: any) => parseFloat(d.NLI))])
-    //   .rangeRound([height - margin.bottom, margin.top])
-    //   .clamp(true);
-
-    let xAxis = g =>
-      g
-        .attr('transform', `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).ticks(width / 80))
-        .call(g => g.select('.domain').remove())
-        .call(g =>
-          g
-            .append('text')
-            .text('Time')
-            .attr('text-anchor', 'end')
-            .attr('x', 390)
-            .attr('fill', 'white')
-            .attr('stroke', 'white')
-            .attr('y', 35),
-        );
-
-    let yAxis = g =>
-      g
-        .attr('transform', `translate(${margin.left},0)`)
-        .call(
-          d3
-            .axisLeft(y)
-            .ticks(null, ',d')
-            .tickFormat(d3.format('.2f')),
-        )
-        .call(g => g.select('.domain').remove())
-        .call(g =>
-          g
-            .append('text')
-            .attr('x', -margin.left)
-            .attr('y', 10)
-            .attr('fill', 'white')
-            .attr('color', 'white')
-            .attr('stroke', 'white')
-            .attr('text-anchor', 'start')
-            .text('NLI'),
-        );
-
-    let grid = g =>
-      g
-        .attr('stroke', 'currentColor')
-        .attr('stroke-opacity', 0.1)
-        .call(g =>
-          g
-            .append('g')
-            .selectAll('line')
-            .data(x.ticks(30))
-            .join('line')
-            .attr('x1', d => 0.5 + x(d))
-            .attr('x2', d => 0.5 + x(d))
-            .attr('y1', margin.top)
-            .attr('y2', height - margin.bottom)
-            .attr('stroke', 'white'),
-        )
-        .call(g =>
-          g
-            .append('g')
-            .selectAll('line')
-            .data(y.ticks(30))
-            .join('line')
-            .attr('y1', d => 0.5 + y(d))
-            .attr('y2', d => 0.5 + y(d))
-            .attr('x1', margin.left)
-            .attr('x2', width - margin.right)
-            .attr('stroke', 'white'),
-        );
-
-    let line = d3
-      .line<NLIConfig>()
-      .x((d: any) => x(new Date(d.NLI_time)))
-      .y((d: any) => y(d.NLI));
-
-    let NLIlimit = d3
-      .line<NLIConfig>()
-      .x((d: any) => x(new Date(d.NLI_time)))
-      .y((d: any) => y(1));
-
-    const svg = nliGraph
-      .append('svg')
-      .attr('viewBox', '-70, -70, 1000, 700')
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', 28)
-      .attr('stroke', 'grey')
-      .attr('stroke-miterlimit', 1);
-
-    svg.append('g').call(xAxis);
-
-    svg.append('g').call(yAxis);
-
-    svg.append('g').call(grid);
-
-    svg
-      .append('path')
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', 4)
-      .attr('d', line(this.LPData.slice(0, this.LPData.length)));
-
-    svg
-      .append('path')
-      .attr('fill', 'none')
-      .attr('stroke', '#98E3CA')
-      .attr('stroke-width', 2)
-      .attr('d', NLIlimit(this.LPData.slice(0, this.LPData.length)));
-
-    return nliDiv;
-  }
-}
